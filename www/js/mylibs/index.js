f987f4bb8997c3ac442abad04beadd4f386cfea4
Chris Dew <cmsdew@gmail.com>
2011-01-25 20:31:14 +0000

    rpc is working

--- www/js/mylibs/index.js
+++ www/js/mylibs/index.js
@@ -7,9 +7,15 @@ $(document).ready(function() {
 	started = true;
 	console.log("index.js started");
     var socket = new io.Socket();
+    window.socket = socket; //debugging
     socket.connect();
     socket.on('connect', function() {
     	var jsonRpc = new JsonRpc(socket, window.CLIENT);
+    	jsonRpc.addMethod("foo", function() {
+    		console.log("foo executed");
+    		return "bar";
+    	});
+    	
     	console.log("sending ping");
     	jsonRpc.rpc('ping', [], function(err, result) {
     		console.log("ping response", err, result);
